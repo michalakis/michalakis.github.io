23543b1b820e05f4d7981e70caf8b6a6490279e5
Michalis Efstathiou <michalis@users.noreply.github.com>
2019-01-21 09:59:43 +0200

    Added changing background and coming soon sign!

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
+// Get body element
 const body = document.querySelector('body');
 
-body.style.backgroundColor = newRGB();
-
-newRGB() {
-    
-    return `rbg( ${value}, ${value}, ${value} )`
+// Generate a random value from 0 to 200
+function newColorValue() {
+    return Math.random() * 200;
 }
+
+// Use the newValue() function to create an rgb value
+function newRGB() {
+    return `rgb( ${ newColorValue() }, ${ newColorValue() }, ${ newColorValue() } )`;
+}
+
+// A function that changes the background color when invoked
+function changeBackgroundColor() {
+    body.style.backgroundColor = newRGB();
+}
+
+window.setInterval(changeBackgroundColor, 3000);
